b3b115b6225569609cc3ccdbf57e93466d86d075
lara <laramsonza@gmail.com>
2024-11-14 16:57:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const cors = require('cors')
+const { Pool } = require('pg')
+
+const app = express()
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'biblioteca',
+    password: 'senai',
+    port: 5432
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/livros', async (req, res) => {
+    try{
+        const result = await pool.query('SELECT * FROM livros');
+        res.json(result.rows)
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({error: "Erro ao buscar livros"})
+    }
+})
+
+
+app.get('/livros/:codigo', async (req,res) =>{
+    const {codigo} = req.params;
+    try{
+        const result = await pool.query('SELECT * FROM livros WHERE codigo = $1', [codigo]) //entra argumento dinamico
+        if(result.rows.length === 0){ //se resultado for zero...
+            return res.status(404).json({error: 'Livro não encontrado'})
+        }
+        res.json(result.rows[0])
+    } catch(err){
+        console.log(err.message);
+        res.status(500).json({error: 'Esse livro não existe'})
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
+
+
+
+
+
